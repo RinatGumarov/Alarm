ab3469f0b9a056d522e63d2b1521ee139c638dff
Tiran678 <tiran678@icloud.com>
2016-03-11 10:06:33 +0300

    onCreate method removed from abstract class BaseActivity

--- app/src/main/java/com/rinat678/alarm/BaseActivity.java
+++ app/src/main/java/com/rinat678/alarm/BaseActivity.java
@@ -1,31 +1,12 @@
 package com.rinat678.alarm;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.ViewConfiguration;
-
-import java.lang.reflect.Field;
 
 public abstract class BaseActivity  extends AppCompatActivity implements android.view.View.OnClickListener{
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        try {
-            ViewConfiguration config = ViewConfiguration.get(this);
-            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
-            if(menuKeyField != null) {
-                menuKeyField.setAccessible(true);
-                menuKeyField.setBoolean(config, false);
-            }
-        } catch (Exception ignore) {
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
